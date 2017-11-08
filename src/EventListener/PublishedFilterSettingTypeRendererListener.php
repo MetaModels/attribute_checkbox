ac793d976bf88e918d70dda46515f15b1422f057
Christian Schiffler <c.schiffler@cyberspectrum.de>
2017-11-08 19:46:34 +0100

    Move scope matching to core

--- src/EventListener/PublishedFilterSettingTypeRendererListener.php
+++ src/EventListener/PublishedFilterSettingTypeRendererListener.php
@@ -13,6 +13,7 @@
  * @package    MetaModels
  * @subpackage AttributeCheckbox
  * @author     David Molineus <david.molineus@netzmacht.de>
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @copyright  2012-2017 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_checkbox/blob/master/LICENSE LGPL-3.0
  * @filesource
@@ -20,46 +21,15 @@
 
 namespace MetaModels\AttributeCheckboxBundle\EventListener;
 
-use ContaoCommunityAlliance\DcGeneral\Contao\RequestScopeDeterminator;
-use ContaoCommunityAlliance\DcGeneral\Contao\View\Contao2BackendView\Event\ModelToLabelEvent;
 use ContaoCommunityAlliance\DcGeneral\Data\ModelInterface;
 use ContaoCommunityAlliance\DcGeneral\EnvironmentInterface;
-use MetaModels\CoreBundle\Assets\IconBuilder;
 use MetaModels\CoreBundle\EventListener\DcGeneral\Table\FilterSetting\AbstractFilterSettingTypeRenderer;
-use MetaModels\Filter\Setting\IFilterSettingFactory;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 /**
  * Handles rendering of model from tl_metamodel_filtersetting.
  */
 class PublishedFilterSettingTypeRendererListener extends AbstractFilterSettingTypeRenderer
 {
-    /**
-     * Request scope determinator.
-     *
-     * @var RequestScopeDeterminator
-     */
-    private $scopeMatcher;
-
-    /**
-     * CreatePropertyConditionListener constructor.
-     *
-     * @param IFilterSettingFactory    $filterSettingFactory The filter factory.
-     * @param EventDispatcherInterface $dispatcher           The event dispatcher.
-     * @param IconBuilder              $iconBuilder          The icon builder.
-     * @param RequestScopeDeterminator $scopeMatcher         Request scope determinator.
-     */
-    public function __construct(
-        IFilterSettingFactory $filterSettingFactory,
-        EventDispatcherInterface $dispatcher,
-        IconBuilder $iconBuilder,
-        RequestScopeDeterminator $scopeMatcher
-    ) {
-        parent::__construct($filterSettingFactory, $dispatcher, $iconBuilder);
-
-        $this->scopeMatcher = $scopeMatcher;
-    }
-
     /**
      * {@inheritdoc}
      */
@@ -74,17 +44,5 @@ class PublishedFilterSettingTypeRendererListener extends AbstractFilterSettingTy
     protected function getLabelParameters(EnvironmentInterface $environment, ModelInterface $model)
     {
         return $this->getLabelParametersWithAttributeAndUrlParam($environment, $model);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function modelToLabel(ModelToLabelEvent $event)
-    {
-        if (!$this->scopeMatcher->currentScopeIsBackend()) {
-            return;
-        }
-
-        parent::modelToLabel($event);
     }
 }
