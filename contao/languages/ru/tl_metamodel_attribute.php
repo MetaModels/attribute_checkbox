d2ebd44610d5dd83ba04bf7908298ba6253ddd50
Mini Model <minimodel@metamodel.me>
2019-01-03 20:12:49 +0100

    Updated translations from transifex.

--- contao/languages/ru/tl_metamodel_attribute.php
+++ contao/languages/ru/tl_metamodel_attribute.php
@@ -1,17 +1,28 @@
 <?php
 /**
- * Translations are managed using Transifex. To create a new translation
+ * This file is part of MetaModels/attribute_checkbox.
+ *
+ * (c) 2012-2019 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * Translations are managed automatically using Transifex. To create a new translation
  * or to help to maintain an existing one, please register at transifex.com.
  *
- * @link http://help.transifex.com/intro/translating.html
- * @link https://www.transifex.com/projects/p/metamodels/language/ru/
+ * Last-updated: 2019-01-03T20:12:39+01:00
  *
- * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
- *
- * last-updated: 2018-02-06T02:49:29+01:00
+ * @copyright 2012-2019 The MetaModels team.
+ * @license   https://github.com/MetaModels/attribute_checkbox/blob/master/LICENSE LGPL-3.0-or-later
+ * @link      https://www.transifex.com/metamodels/public/
+ * @link      https://www.transifex.com/signup/?join_project=metamodels
  */
 
+
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['check_inverse']['0']              = 'Обратный';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['check_inverse']['1']              = 'Если этот параметр выбран, опубликованный режим будет инвертирован (например, элементы контента).';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['check_listview']['0']             = 'ListView чекбокс';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['check_listview']['1']             = 'Если параметр выбран, вы получите дополнительный значок в панели управления listview.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['check_listviewicon']['0']         = 'Значок ListView';
