66d9ead9aeccb3a14d76bbbd435cb11dec013c30
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2020-03-27 16:41:36 +0100

    Fix CS

--- src/FilterSetting/Published.php
+++ src/FilterSetting/Published.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/attribute_checkbox.
  *
- * (c) 2012-2019 The MetaModels team.
+ * (c) 2012-2020 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -16,7 +16,7 @@
  * @author     Sven Baumann <baumann.sv@gmail.com>
  * @author     David Molineus <david.molineus@netzmacht.de>
  * @author     Richard Henkenjohann <richardhenkenjohann@googlemail.com>
- * @copyright  2012-2019 The MetaModels team.
+ * @copyright  2012-2020 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_checkbox/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -84,7 +84,7 @@ class Published extends Simple
             $connection = System::getContainer()->get('database_connection');
         }
 
-        $this->connection = $connection;
+        $this->connection   = $connection;
         $this->tokenChecker = $tokenChecker;
     }
 
