ac37d567ca862bddf8cd0b7b66cf698cc9892b9a
e-spin <gitlab@e-spin.de>
2020-03-30 11:33:39 +0200

    cast reserved words

--- src/FilterSetting/Published.php
+++ src/FilterSetting/Published.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/attribute_checkbox.
  *
- * (c) 2012-2019 The MetaModels team.
+ * (c) 2012-2020 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -15,7 +15,8 @@
  * @author     Andreas Isaak <info@andreas-isaak.de>
  * @author     Sven Baumann <baumann.sv@gmail.com>
  * @author     David Molineus <david.molineus@netzmacht.de>
- * @copyright  2012-2019 The MetaModels team.
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2012-2020 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_checkbox/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -105,8 +106,8 @@ class Published extends Simple
                     ->connection
                     ->createQueryBuilder()
                     ->select('id')
-                    ->from($this->getMetaModel()->getTableName())
-                    ->where($objAttribute->getColName() . '=:value')
+                    ->from($this->getMetaModel()->getTableName(), 't')
+                    ->where('t.' . $objAttribute->getColName() . '=:value')
                     ->setParameter('value', $publishedValue)
             ));
 
