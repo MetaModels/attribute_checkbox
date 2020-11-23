54801e53b9d1c792ae5fac6b86cc6dfe7c30d694
zonky2 <info@e-spin.de>
2020-11-23 11:42:52 +0100

    Fix DCA metapalettes configuration

--- src/Resources/contao/dca/tl_metamodel_attribute.php
+++ src/Resources/contao/dca/tl_metamodel_attribute.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/attribute_checkbox.
  *
- * (c) 2012-2019 The MetaModels team.
+ * (c) 2012-2020 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -19,7 +19,7 @@
  * @author     Sven Baumann <baumann.sv@gmail.com>
  * @author     David Molineus <david.molineus@netzmacht.de>
  * @author     Ingolf Steinhardt <info@e-spin.de>
- * @copyright  2012-2019 The MetaModels team.
+ * @copyright  2012-2020 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_checkbox/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -28,9 +28,8 @@ $GLOBALS['TL_DCA']['tl_metamodel_attribute']['metapalettes']['checkbox extends _
     '+advanced' => ['check_publish', 'check_inverse', 'check_listview']
 ];
 
-$GLOBALS['TL_DCA']['tl_metamodel_attribute']['metasubpalettes'] = [
-    'check_listview' => ['check_listviewicon', 'check_listviewicondisabled']
-];
+$GLOBALS['TL_DCA']['tl_metamodel_attribute']['metasubpalettes']['check_listview'] =
+    ['check_listviewicon', 'check_listviewicondisabled'];
 
 $GLOBALS['TL_DCA']['tl_metamodel_attribute']['fields']['check_publish'] = [
     'label'     => &$GLOBALS['TL_LANG']['tl_metamodel_attribute']['check_publish'],
